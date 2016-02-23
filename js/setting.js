7d3915ee764555efa79f617843b443b19f0cfecb
Norio Miyake <miyake@MacBook-Pro15.local>
2016-02-23 15:15:12 +0900

    Initial commit

---
+++ js/setting.js
@@ -0,0 +1,32 @@
+/*
+ * Icons for COFOG taxonomy
+ *
+ */
+var Taxes = Taxes || {};
+
+Taxes.baseKoujo = 330000; // 住民税基礎控除
+Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.taxRate = 0.06; // 住民税率
+
+var OpenSpending = OpenSpending || {};
+
+OpenSpending.identifier = 'yakage_2015_ippan';
+OpenSpending.year = '2015';
+
+OpenSpending.Styles = OpenSpending.Styles || {};
+
+OpenSpending.Styles.Cofog = {
+
+  /* yokohama cofog */
+  '1': { icon: 'icons/family2.svg', color: '#C75746', bcolor: '#935B3B' },
+  '2': { icon: 'icons/manufactoring-construction.svg', color: '#C75746', bcolor: '#0AB971' },
+  '3': { icon: 'icons/publicaffairs.svg', color: '#C75746', bcolor: '#4E6D00' },
+  '4': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#D33673' },
+  '5': { icon: 'icons/education.svg', color: '#C75746', bcolor: '#2A3A03' },
+  '6': { icon: 'icons/farms.svg', color: '#C75746', bcolor: '#EC2406' },
+  '7': { icon: 'icons/money.svg', color: '#C75746', bcolor: '#938626' },
+  '8': { icon: 'icons/fire-brigade.svg', color: '#C75746', bcolor: '#C75746' },
+  '9': { icon: 'icons/economic-aid.svg', color: '#C75746', bcolor: '#D33673' },
+  '10': { icon: 'icons/government-uk.svg', color: '#C75746', bcolor: '#790586' },
+  '11': { icon: 'icons/admin.svg', color: '#C75746', bcolor: '#790586' },
+};
